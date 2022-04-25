26d7899de554c3d1d9d161eaf56b13ac6aa0be6c
Gustavo Ernesto H. Corvera <guzguztavo91@gmail.com>
2022-04-24 19:28:10 -0600

    Importaciones de librerias en el mainPrincipal

--- app/src/main/java/sv/ues/fia/eisi/proyecto1/MainActivity.java
+++ app/src/main/java/sv/ues/fia/eisi/proyecto1/MainActivity.java
@@ -1,13 +1,16 @@
 package sv.ues.fia.eisi.proyecto1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends ListActivity {
-
+    String[] menu = {"Tabla Cliente"};
+    String[] activities = {"ClienteMenuActivity"};
     BD_Controlador BDhelper;
 
     @Override
@@ -16,4 +19,27 @@ public class MainActivity extends ListActivity {
         setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, menu));
         BDhelper=new BD_Controlador(this);
     }
+
+    /*
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        if (position != 3) {
+            String nombreValue = activities[position];
+            try {
+                Class<?> clase = Class.forName("sv.ues.fia.eisi.guia4." + nombreValue);
+                Intent inte = new Intent(this, clase);
+                this.startActivity(inte);
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        } else {
+            //CODIGO PARA LLENAR BASE DE DATOS
+            BDhelper.abrir();
+            String tost=BDhelper.llenarBDCarnet();
+            BDhelper.cerrar();
+            Toast.makeText(this, tost, Toast.LENGTH_SHORT).show();
+        }
+    }
+    */
 }
