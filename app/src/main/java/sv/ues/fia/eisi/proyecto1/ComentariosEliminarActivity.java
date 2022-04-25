94da97c1f20b4476ee0293d47b7da4d0df77e80d
Gustavo Ernesto H. Corvera <guzguztavo91@gmail.com>
2022-04-24 23:23:16 -0600

    Creación del layout EliminarComentario, así como su creación de atributos en la clase. enlazando sus campos

--- app/src/main/java/sv/ues/fia/eisi/proyecto1/ComentariosEliminarActivity.java
+++ app/src/main/java/sv/ues/fia/eisi/proyecto1/ComentariosEliminarActivity.java
@@ -1,25 +1,23 @@
 package sv.ues.fia.eisi.proyecto1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
-public class ComentariosEliminarActivity extends AppCompatActivity {
+public class ComentariosEliminarActivity extends Activity {
+    BD_Controlador helper;
+    EditText editIdComentario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_comentarios_eliminar);
-
-
+        setContentView(R.layout.activity_comentarios_consultar);
+        helper = new BD_Controlador(this);
+        editIdComentario = (EditText) findViewById(R.id.editIdComentario);
     }
 
     public void eliminarComentarios(View v){
-
-    }
-
-    public void limpiarTexto(View v){
 
     }
 }
