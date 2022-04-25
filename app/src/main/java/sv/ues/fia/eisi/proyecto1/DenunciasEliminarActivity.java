56c7bc78b0eef0a6d5d162e170ebb0b444b45fc3
Gustavo Ernesto H. Corvera <guzguztavo91@gmail.com>
2022-04-24 23:48:17 -0600

    Creación del layout EliminarDenuncia, así como su creación de atributos en la clase. enlazando sus campos

--- app/src/main/java/sv/ues/fia/eisi/proyecto1/DenunciasEliminarActivity.java
+++ app/src/main/java/sv/ues/fia/eisi/proyecto1/DenunciasEliminarActivity.java
@@ -1,26 +1,23 @@
 package sv.ues.fia.eisi.proyecto1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class DenunciasEliminarActivity extends Activity {
+    BD_Controlador helper;
+    EditText editIdDenuncia;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_denuncias_eliminar);
-
-
+        helper = new BD_Controlador(this);
+        editIdDenuncia = (EditText) findViewById(R.id.editIdDenuncia);
     }
 
     public void eliminarDenuncias(View v){
-
-    }
-
-    public void limpiarTexto(View v){
 
     }
 }
