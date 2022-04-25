2fe618f4aee94f8a2e6e49dd4142ee97b46a9218
Gustavo Ernesto H. Corvera <guzguztavo91@gmail.com>
2022-04-25 10:36:43 -0600

    Creación del layout consultarEvaluacion, así como su creación de atributos en la clase. enlazando sus campos

--- app/src/main/java/sv/ues/fia/eisi/proyecto1/EvaluacionConsultarActivity.java
+++ app/src/main/java/sv/ues/fia/eisi/proyecto1/EvaluacionConsultarActivity.java
@@ -1,17 +1,28 @@
 package sv.ues.fia.eisi.proyecto1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class EvaluacionConsultarActivity extends Activity {
+    BD_Controlador helper;
+    EditText editIdEvaluacion;
+    EditText editIdLocal;
+    EditText editIdCliente;
+    EditText editIdTipoSatisfaccion;
+    EditText editNotaEvaCliente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_evaluacion_consultar);
+        helper = new BD_Controlador(this);
+        editIdEvaluacion = (EditText) findViewById(R.id.editIdEvaluacion);
+        editIdLocal = (EditText) findViewById(R.id.editIdLocal);
+        editIdCliente = (EditText) findViewById(R.id.editIdCliente);
+        editIdTipoSatisfaccion = (EditText) findViewById(R.id.editIdTipoSatisfaccion);
+        editNotaEvaCliente = (EditText) findViewById(R.id.editNotaEvaCliente);
     }
 
     public void consultarEvaluacion(View v){
