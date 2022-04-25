3f1e5d6571f2543eb39699a48b25551dd766f722
Gustavo Ernesto H. Corvera <guzguztavo91@gmail.com>
2022-04-24 23:26:30 -0600

    Creación del layout InsertarComentario, así como su creación de atributos en la clase. enlazando sus campos

--- app/src/main/java/sv/ues/fia/eisi/proyecto1/ComentariosInsertarActivity.java
+++ app/src/main/java/sv/ues/fia/eisi/proyecto1/ComentariosInsertarActivity.java
@@ -1,19 +1,28 @@
 package sv.ues.fia.eisi.proyecto1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class ComentariosInsertarActivity extends Activity {
+    BD_Controlador helper;
+    EditText editIdComentario;
+    EditText editIdUsuarioComentario;
+    EditText editIdLocal;
+    EditText editTextComentario;
+    EditText editFechaComentario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comentarios_insertar);
-
-
+        helper = new BD_Controlador(this);
+        editIdComentario = (EditText) findViewById(R.id.editIdComentario);
+        editIdUsuarioComentario = (EditText) findViewById(R.id.editIdUsuarioComentario);
+        editIdLocal = (EditText) findViewById(R.id.editIdLocalComentario);
+        editTextComentario = (EditText) findViewById(R.id.editTextComentario);
+        editFechaComentario = (EditText) findViewById(R.id.editFechaComentario);
     }
 
     public void insertarComentarios(View v){
