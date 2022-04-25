314d1f8f3df9d89d7aed64330c48c9d4cf583f66
Gustavo Ernesto H. Corvera <guzguztavo91@gmail.com>
2022-04-25 00:14:41 -0600

    Creación del layout ElminarSugerencia, así como su creación de atributos en la clase. enlazando sus campos

--- app/src/main/java/sv/ues/fia/eisi/proyecto1/SugerenciasEliminarActivity.java
+++ app/src/main/java/sv/ues/fia/eisi/proyecto1/SugerenciasEliminarActivity.java
@@ -1,24 +1,23 @@
 package sv.ues.fia.eisi.proyecto1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class SugerenciasEliminarActivity extends Activity {
+    BD_Controlador helper;
+    EditText editIdSugerencia;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sugerencias_eliminar);
+        helper = new BD_Controlador(this);
+        editIdSugerencia = (EditText) findViewById(R.id.editIdSugerencia);
     }
 
     public void eliminarSugerencias(View v){
-
-    }
-
-    public void limpiarTexto(View v){
 
     }
 }
