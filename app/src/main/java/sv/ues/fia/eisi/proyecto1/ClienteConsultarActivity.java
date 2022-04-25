92b93294f415853807ad51c3190c569d82a08092
Gustavo Ernesto H. Corvera <guzguztavo91@gmail.com>
2022-04-24 22:17:22 -0600

    Creación del layout ConsultarCliente, así como su creación de atributos en la clase. enlazando sus campos

--- app/src/main/java/sv/ues/fia/eisi/proyecto1/ClienteConsultarActivity.java
+++ app/src/main/java/sv/ues/fia/eisi/proyecto1/ClienteConsultarActivity.java
@@ -1,19 +1,31 @@
 package sv.ues.fia.eisi.proyecto1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class ClienteConsultarActivity extends Activity {
+    BD_Controlador helper;
+    EditText editIdCliente;
+    EditText editIdRangoEdad;
+    EditText editIdUsuario;
+    EditText editIdSexo;
+    EditText editNomcliente;
+    EditText editTelefonoCliente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cliente_consultar);
-
-
+        helper = new BD_Controlador(this);
+        editIdCliente = (EditText) findViewById(R.id.editIdCliente);
+        editIdRangoEdad = (EditText) findViewById(R.id.editIdRangoEdad);
+        editIdUsuario = (EditText) findViewById(R.id.editIdUsuario);
+        editIdSexo = (EditText) findViewById(R.id.editIdSexo);
+        editNomcliente = (EditText) findViewById(R.id.editNomCliente);
+        editTelefonoCliente = (EditText) findViewById(R.id.editTelefonoCliente);
     }
 
     public void consultarCliente (View v){
