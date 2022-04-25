9521f68544a1ac0c50a49a71a1f1badbcc186214
Gustavo Ernesto H. Corvera <guzguztavo91@gmail.com>
2022-04-25 14:28:10 -0600

    Creación del CRUD Sector

--- app/src/main/java/sv/ues/fia/eisi/proyecto1/SectorActualizarActivity.java
+++ app/src/main/java/sv/ues/fia/eisi/proyecto1/SectorActualizarActivity.java
@@ -1,17 +1,22 @@
 package sv.ues.fia.eisi.proyecto1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class SectorActualizarActivity extends Activity {
+    BD_Controlador helper;
+    EditText editIdSector;
+    EditText editTipoSector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sector_actualizar);
+        helper = new BD_Controlador(this);
+        editIdSector = (EditText) findViewById(R.id.editIdSector);
+        editTipoSector = (EditText) findViewById(R.id.editTipoSector);
     }
 
     public void actualizarSector(View v){
--- app/src/main/java/sv/ues/fia/eisi/proyecto1/SectorEliminarActivity.java
+++ app/src/main/java/sv/ues/fia/eisi/proyecto1/SectorEliminarActivity.java
@@ -1,17 +1,20 @@
 package sv.ues.fia.eisi.proyecto1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class SectorEliminarActivity extends Activity {
+    BD_Controlador helper;
+    EditText editIdSector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sector_eliminar);
+        helper = new BD_Controlador(this);
+        editIdSector = (EditText) findViewById(R.id.editIdSector);
     }
 
     public void eliminarSector(View v){
