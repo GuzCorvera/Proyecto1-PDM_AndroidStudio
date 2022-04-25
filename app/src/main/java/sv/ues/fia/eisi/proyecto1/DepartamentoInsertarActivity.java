7f857c10f70fcf722714d68d0dee1701cfffe94a
Gustavo Ernesto H. Corvera <guzguztavo91@gmail.com>
2022-04-25 00:48:03 -0600

    Creación del layout InsertarDepartamento, así como su creación de atributos en la clase. enlazando sus campos

--- app/src/main/java/sv/ues/fia/eisi/proyecto1/DepartamentoInsertarActivity.java
+++ app/src/main/java/sv/ues/fia/eisi/proyecto1/DepartamentoInsertarActivity.java
@@ -5,13 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class DepartamentoInsertarActivity extends Activity {
+    BD_Controlador helper;
+    EditText editIdDepartamento;
+    EditText editNomDepartamento;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_departamento_insertar);
+        helper = new BD_Controlador(this);
+        editIdDepartamento = (EditText) findViewById(R.id.editIdDepartamento);
+        editNomDepartamento = (EditText) findViewById(R.id.editNomDepartamento);
     }
 
     public void insertarDepartamento(View v){
