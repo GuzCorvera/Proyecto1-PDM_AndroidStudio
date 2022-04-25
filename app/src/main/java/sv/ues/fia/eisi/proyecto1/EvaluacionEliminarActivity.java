d9d464d816ae6fe5ab2ac2885c13c50e147e973c
Gustavo Ernesto H. Corvera <guzguztavo91@gmail.com>
2022-04-25 10:42:25 -0600

    Creación del layout eliminarEvaluacion, así como su creación de atributos en la clase. enlazando sus campos

--- app/src/main/java/sv/ues/fia/eisi/proyecto1/EvaluacionEliminarActivity.java
+++ app/src/main/java/sv/ues/fia/eisi/proyecto1/EvaluacionEliminarActivity.java
@@ -1,17 +1,20 @@
 package sv.ues.fia.eisi.proyecto1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class EvaluacionEliminarActivity extends Activity {
+    BD_Controlador helper;
+    EditText editIdEvaluacion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_evaluacion_eliminar);
+        helper = new BD_Controlador(this);
+        editIdEvaluacion = (EditText) findViewById(R.id.editIdEvaluacion);
     }
 
     public void eliminarEvaluacion(View v){
