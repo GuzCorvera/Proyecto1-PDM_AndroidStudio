0ab55361522c8752d93ae7d7f00d40b506e42768
Gustavo Ernesto H. Corvera <guzguztavo91@gmail.com>
2022-04-25 00:10:16 -0600

    Creación del layout ConsultarSugerencia, así como su creación de atributos en la clase. enlazando sus campos

--- app/src/main/java/sv/ues/fia/eisi/proyecto1/SugerenciasConsultarActivity.java
+++ app/src/main/java/sv/ues/fia/eisi/proyecto1/SugerenciasConsultarActivity.java
@@ -1,17 +1,28 @@
 package sv.ues.fia.eisi.proyecto1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class SugerenciasConsultarActivity extends Activity {
+    BD_Controlador helper;
+    EditText editIdSugerencia;
+    EditText editIdUsuarioSugerencia;
+    EditText editIdLocalSugerencia;
+    EditText editTextSugerencia;
+    EditText editFechaSugerencia;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sugerencias_consultar);
+        helper = new BD_Controlador(this);
+        editIdSugerencia = (EditText) findViewById(R.id.editIdSugerencia);
+        editIdUsuarioSugerencia = (EditText) findViewById(R.id.editIdUsuarioSugerencia);
+        editIdLocalSugerencia = (EditText) findViewById(R.id.editIdLocalSugerencia);
+        editTextSugerencia = (EditText) findViewById(R.id.editTextSugerencia);
+        editFechaSugerencia = (EditText) findViewById(R.id.editFechaSugerencia);
     }
 
     public void consultarSugerencias(View v){
