27e6cbaf09562bbdf6ad522712769b7aefdffd0c
Gustavo Ernesto H. Corvera <guzguztavo91@gmail.com>
2022-04-25 00:18:53 -0600

    Creación del layout InsertarSugerencia, así como su creación de atributos en la clase. enlazando sus campos

--- app/src/main/java/sv/ues/fia/eisi/proyecto1/SugerenciasInsertarActivity.java
+++ app/src/main/java/sv/ues/fia/eisi/proyecto1/SugerenciasInsertarActivity.java
@@ -1,19 +1,29 @@
 package sv.ues.fia.eisi.proyecto1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class SugerenciasInsertarActivity extends Activity {
+    BD_Controlador helper;
+    EditText editIdSugerencia;
+    EditText editIdUsuarioSugerencia;
+    EditText editIdLocalSugerencia;
+    EditText editTextSugerencia;
+    EditText editFechaSugerencia;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sugerencias_insertar);
+        helper = new BD_Controlador(this);
+        editIdSugerencia = (EditText) findViewById(R.id.editIdSugerencia);
+        editIdUsuarioSugerencia = (EditText) findViewById(R.id.editIdUsuarioSugerencia);
+        editIdLocalSugerencia = (EditText) findViewById(R.id.editIdLocalSugerencia);
+        editTextSugerencia= (EditText) findViewById(R.id.editTextSugerencia);
+        editFechaSugerencia = (EditText) findViewById(R.id.editFechaSugerencia);
     }
-
     public void insertarSugerencias(View v){
 
     }
