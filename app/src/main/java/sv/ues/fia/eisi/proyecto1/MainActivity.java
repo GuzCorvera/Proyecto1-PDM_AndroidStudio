b4163fef5181df7e7c343a0c00de74011738f47e
Gustavo Ernesto H. Corvera <guzguztavo91@gmail.com>
2022-04-25 21:05:03 -0600

    Creación de los menú de las tablas enlazadas con su CRUD funcionalidad al 100%

--- app/src/main/java/sv/ues/fia/eisi/proyecto1/MainActivity.java
+++ app/src/main/java/sv/ues/fia/eisi/proyecto1/MainActivity.java
@@ -10,8 +10,45 @@ import android.widget.Toast;
 
 public class MainActivity extends ListActivity {
     //String[] menu = {"Tabla Cliente","Tabla Comentario","Denuncia","LLenar Base de Datos"};
-    String[] menu = {"Tabla Cliente", "Tabla Comentario"};
-    String[] activities = {"ClienteMenuActivity"};
+    String[] menu = {"" +
+            "Tabla Cliente",
+            "Tabla Comentarios",
+            "Tabla Denuncias",
+            "Tabla Departamento",
+            "Tabla Empresa",
+            "Tabla Evaluación",
+            "Tabla Local",
+            "Tabla Municipio",
+            "Tabla Rango de Edad",
+            "Tabla Sector",
+            "Tabla Sexo",
+            "Tabla Sugerencias",
+            "Tabla Tipo de Empresa",
+            "Tabla Tipo de Satisfacción",
+            "Tabla Tipo de Usuario",
+            "Tabla Usuario"};
+    String[] activities = {
+            "ClienteMenuActivity",
+            "ComentarioMenuActivity",
+            "DenunciasMenuActivity",
+            "DepartamentoMenuActivity",
+            "EmpresaMenuActivity",
+            "EvaluacionMenuActivity",
+            "LocalMenuActivity",
+            "MunicipioMenuActivity",
+            "Rango_EdadMenuActivity",
+            "SectorMenuActivity",
+            "SexoMenuActivity",
+            "SugerenciasMenuActivity",
+            "Tipo_EmpresaMenuActivity",
+            "Tipo_SatisfaccionMenuActivity",
+            "Tipo_UsuarioMenuActivity",
+            "UsuarioMenuActivity"
+            // LA POSICION 16 SERIA LA OPCION LLENAR BASE DE DATOS
+            // LA POSICION 16 SERIA LA OPCION LLENAR BASE DE DATOS
+            // LA POSICION 16 SERIA LA OPCION LLENAR BASE DE DATOS
+            // LA POSICION 16 SERIA LA OPCION LLENAR BASE DE DATOS
+    };
     BD_Controlador BDhelper;
 
     @Override
@@ -24,7 +61,7 @@ public class MainActivity extends ListActivity {
     @Override
     protected void onListItemClick(ListView l, View v, int position, long id) {
         super.onListItemClick(l, v, position, id);
-        if (position != 3) {
+        if (position != 16) {// LA POSICION 16 SERIA LA OPCION LLENAR BASE DE DATOS
             String nombreValue = activities[position];
             try {
                 Class<?> clase = Class.forName("sv.ues.fia.eisi.proyecto1." + nombreValue);
---
+++ app/src/main/java/sv/ues/fia/eisi/proyecto1/Tipo_SatisfaccionMenuActivity.java
@@ -0,0 +1,39 @@
+package sv.ues.fia.eisi.proyecto1;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class Tipo_SatisfaccionMenuActivity extends ListActivity {
+    String[] menu = {"Insertar Tipo de Satisfacción", "Eliminar Tipo de Satisfacción", "Consultar Tipo de Satisfacción", "Actualizar Tipo de Satisfacción"};
+    String[] activities = {"Tipo_SatisfaccionInsertarActivity", "Tipo_SatisfaccionEliminarActivity", "Tipo_SatisfaccionConsultarActivity", "Tipo_SatisfaccionActualizarActivity"};
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ListView listView = getListView();
+        listView.setBackgroundColor(Color.rgb(0, 0, 255));
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, menu);
+        setListAdapter(adapter);
+    }
+
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        String nombreValue = activities[position];
+        l.getChildAt(position).setBackgroundColor(Color.rgb(128, 128, 255));
+
+        try {
+            Class<?> clase = Class.forName("sv.ues.fia.eisi.proyecto1." + nombreValue);
+            Intent inte = new Intent(this, clase);
+            this.startActivity(inte);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
